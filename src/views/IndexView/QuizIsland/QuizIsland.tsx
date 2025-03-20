369072045bb8e1bc52963fb20a39863c63aecc70
edard3v <edard3v@gmail.com>
2025-03-20 03:07:18 -0500

    refactor

--- src/views/IndexView/QuizIsland/QuizIsland.tsx
+++ src/views/IndexView/QuizIsland/QuizIsland.tsx
@@ -1,9 +1,3 @@
-import LiteYoutube from "@solid/components/shared/LiteYoutube/LiteYoutube";
-
 export default function QuizIsland() {
-  return (
-    <div>
-      <LiteYoutube video_id="mqDg5Oi8S_E" />
-    </div>
-  );
+  return <div>quiz</div>;
 }
